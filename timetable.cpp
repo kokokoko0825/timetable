96e1600767fec38033db21051aaad8e855150c20
r03i22 <103725932+r03i22@users.noreply.github.com>
2024-12-10 15:47:26 +0900

    Impl: timetable md

--- timetable.cpp
+++ timetable.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
-void timetable( int first, int second ){
-    for (int i = 0; i < 16; i++)
+void timetable( int first, int second, ofstream &outfile){
+    outfile << "|時間| |"<< endl;
+    outfile << "|:-:|:-:|"<< endl;
+    outfile << "|" << setw(2) << setfill('0') << first << ":" << setw(2) << setfill('0') << second << "|" << "|" << endl;
+    for (int i = 0; i < 5; i++)
     {
         second += 30; //ライブ時間に合わせて別途変更
         if (second >= 60){
             first += 1;
             second -= 60;
         }
-        cout << "~" << setw(2) << setfill('0') << first << ":" << setw(2) << setfill('0') << second << "   :  バンド" << i+1 << endl;
+        outfile << "|" << "~" << setw(2) << setfill('0') << first << ":" << setw(2) << setfill('0') << second << "|" << "バンド" << i+1 << "|" << endl;
         second += 15; //転換時間に合わせて別途変更
         if (second >= 60){
             first += 1;
             second -= 60;
         }
-        cout << "~" << setw(2) << setfill('0') << first << ":" << setw(2) << setfill('0') << second << "   :  転換" << endl;
+        outfile << "|" << "~" << setw(2) << setfill('0') << first << ":" << setw(2) << setfill('0') << second << "|" << "転換" << "|" << endl;
     }
 };
 
@@ -26,6 +30,12 @@ int main(){
     char colon;
     cout << "演奏の開始時刻を入力してください。";
     cin >> first >> colon >> second;
-    timetable(first, second);
+    ofstream outfile("timetable.md");
+    if (!outfile){
+        cout << "ファイルを開けませんでした。";
+        return 1;
+    }
+    timetable(first, second, outfile);
+    outfile.close();
     return 0;
 }
