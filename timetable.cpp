e635256950d894d69e5bba6dea33118f11091aa1
r03i22 <103725932+r03i22@users.noreply.github.com>
2024-12-16 11:54:51 +0900

    style: timetable design

--- timetable.cpp
+++ timetable.cpp
@@ -3,11 +3,11 @@
 #include <fstream>
 using namespace std;
 
-void rehatable(int hour, int minuit, ofstream &outfile, int band, int rehatime, int rehachangetime){
+void rehatable(int &hour, int &minuit, ofstream &outfile, int band, int rehatime, int rehachangetime){
     outfile << "|時間| |"<< endl;
     outfile << "|:-:|:-:|"<< endl;
-    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "|" << endl;
-    outfile << "|" ;
+    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "| リハ START |" << endl;
+    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
     for (int i = band; i > 0; i--)
     {
         minuit += rehatime; //バンドリハの演奏時間
@@ -16,22 +16,28 @@ void rehatable(int hour, int minuit, ofstream &outfile, int band, int rehatime, 
             minuit -= 60;
         }
         outfile << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "バンド" << i << "|" << endl;
-        outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
-
-        minuit += rehachangetime; //リハ転換時間
-        if (minuit >= 60){
-            hour += 1;
-            minuit -= 60;
+        if(i != 1){
+            outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
         }
-        if(rehachangetime != 0){
-        outfile << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "転換" << "|" << endl;
-        outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
+        if(i != 1){
+            minuit += rehachangetime; //リハ転換時間
+            if (minuit >= 60){
+                hour += 1;
+                minuit -= 60;
+            }
+            if(rehachangetime != 0){
+            outfile << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "転換" << "|" << endl;
+            outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
+            }
+        } else {
+            outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "リハ終了" << "|" << endl;
         }
     }
 };
 
 void timetable(int hour, int minuit, ofstream &outfile, int band, int bandtime, int changetime){
-    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "|" << endl;
+    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "| ライブ START |" << endl;
+    outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
     for (int i = 0; i < band; i++)
     {
         minuit += bandtime; //バンドの演奏時間
@@ -39,7 +45,8 @@ void timetable(int hour, int minuit, ofstream &outfile, int band, int bandtime, 
             hour += 1;
             minuit -= 60;
         }
-        outfile << "|" << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "バンド" << i+1 << "|" << endl;
+        outfile << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "バンド" << i+1 << "|" << endl;
+        outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
 
         minuit += changetime; //転換時間
         if (minuit >= 60){
@@ -47,7 +54,12 @@ void timetable(int hour, int minuit, ofstream &outfile, int band, int bandtime, 
             minuit -= 60;
         }
         if(changetime != 0){
-        outfile << "|" << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "転換" << "|" << endl;
+        outfile << "~" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit << "|" << "転換" << "|" << endl;
+        outfile << "|" << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minuit ;
+        }
+
+        if(i == band-1){
+            outfile << "|" << "終了" << "|" << endl;
         }
     }
 };
@@ -59,7 +71,7 @@ int main(){
     char colon;
     int band = 8;  //バンド数によって変更
     int rehatime = 30; //リハの時間に合わせて変更
-    int rehachangetime = 0; //リハの転換時間に合わせて変更
+    int rehachangetime = 15; //リハの転換時間に合わせて変更
     int bandtime = 30; //ライブ時間に合わせて変更
     int changetime = 15; //転換時間に合わせて変更
     cout << "演奏の開始時刻を入力してください。ex.(HH:MM) ->";
