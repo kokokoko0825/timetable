be8470150556ab2e75bafbfddd2ac584e7d9ca41
r03i22 <103725932+r03i22@users.noreply.github.com>
2024-12-17 17:31:23 +0900

    feat: upload web

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,5 +7,11 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
 };
